948a44d1e0957fbdeef94f4ea26e34105169e755
Jesus <133887362+JessEleven@users.noreply.github.com>
2024-11-12 11:40:00 -0500

    RESTful operations for users have been added

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -3,51 +3,137 @@
 namespace App\Http\Controllers;
 
 use App\Models\Usuario;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class UsuarioController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
+    private $rulesUser = array(
+        'nombre'=> 'required',
+        'apellido'=> 'required',
+        'telefono'=> 'required|',
+        'email'=> 'required|',
+        'password'=> 'required',
+        'id_area'=> 'required',
+        'id_departamento'=> 'required'
+    );
+
+    private $rulesUserUpdate = array(
+        'nombre'=> 'required',
+        'apellido'=> 'required',
+        'telefono'=> 'required|numeric',
+        'email'=> 'required',
+        'password'=> 'required',
+        'id_area'=> 'required',
+        'id_departamento'=> 'required'
+    );
+
+    private $messagesUser = array(
+        'nombre.required'=> 'El nombre es requerido',
+        'apellido.required'=> 'El apellido es requerido',
+        'telefono.required'=> 'El telefono es requerido',
+        'email.required'=> 'El correo es requerido',
+        'password.required'=> 'La contraseña es requerido',
+        'id_area'=> 'El id area es requerido',
+        'id_departamento'=> 'El id departamento es requerido',
+    );
+
     public function index()
     {
-        $usuario = Usuario::all();
-        return response()->json([
-            "usuario_junin"=> $usuario
-        ]);
+        try {
+            $allUsers = Usuario::with('areas', 'departamentos')->get();
+            return response()->json(["Total de usuarios ". $allUsers->count(), $allUsers]);
+
+        } catch (\Exception $e) {
+            return response()->json(["error"=> $e->getMessage()], 500);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), $this->rulesUser, $this->messagesUser);
+
+        if ($validator->fails()) {
+            $messages = $validator->messages();
+            return response()->json(["messages"=> $messages], 500);
+        }
+
+        try {
+            $newUser=Usuario::create([
+                "nombre" => $request->nombre,
+                "apellido"=> $request->apellido,
+                "telefono"=> $request->telefono,
+                "email"=> $request->email,
+                "password"=> $request->password,
+                "id_area" => $request->id_area,
+                "id_departamento"=> $request->id_departamento,
+            ]);
+
+            return response()->json([
+                "message"=> "Usuario creado con exito", $newUser], 200);
+
+        } catch (\Exception $e) {
+            return response()->json(['error'=> $e->getMessage()], 500);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Usuario $usuario)
+    public function show($usuario)
     {
-        //
+        try {
+            $showUser = Usuario::whereId($usuario)->with('areas', 'departamentos')->findOrFail($usuario);
+            return response()->json([
+                "message"=> "Usuario encontrado con exito", $showUser], 200);
+
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error'=> 'Usuario no encontrado'], 404);
+
+        } catch (\Exception $e) {
+            return response()->json(['error'=> $e->getMessage()], 500);
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Usuario $usuario)
+    public function update(Request $request, $usuario)
     {
-        //
+        $validator = Validator::make($request->all(), $this->rulesUserUpdate, $this->messagesUser);
+
+        if ($validator->fails()) {
+            $messages = $validator->messages();
+            return response()->json(["messages"=> $messages], 500);
+        }
+
+        try {
+            Usuario::whereId($usuario)->findOrFail($usuario)->update([
+                "nombre"=> $request->nombre,
+                "apellido"=> $request->apellido,
+                "telefono"=> $request->telefono,
+                "email"=> $request->email,
+                "password"=> $request->password,
+                "id_area" => $request->id_area,
+                "id_departamento"=> $request->id_departamento,
+            ]);
+            return response()->json([
+                "message"=> "Usuario actualizado con exito"], 200);
+
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error'=> 'Usuario no encontrado'], 404);
+        }
+
+        catch (\Exception $e) {
+            return response()->json(['error'=> $e->getMessage()], 500);
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Usuario $usuario)
+    public function destroy($usuario)
     {
-        //
+        try {
+            Usuario::findOrFail($usuario)->delete();
+            return response()->json([
+                "message"=> "Usuario eliminado con exito"], 200);
+
+        } catch (\Exception $e) {
+            return response()->json(['error'=> $e->getMessage()], 500);
+        }
     }
 }

1c05daeb93d870c3fad9ee389eb09d3720e3ae84
Jesus <133887362+JessEleven@users.noreply.github.com>
2024-11-25 19:25:13 -0500

    adding versioning to the API

--- routes/api.php
+++ routes/api.php
@@ -15,21 +15,21 @@ use App\Http\Controllers\TicketController;
 use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\UsuarioRolController;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('area', AreaController::class);
-Route::apiResource('bitacora', BitacoraTicketController::class);
-Route::apiResource('calificacion-ticket', CalificacionTicketController::class);
-Route::apiResource('categoria-ticket', CategoriaTicketController::class);
-Route::apiResource('departamento', DepartamentoController::class);
-Route::apiResource('estado-ticket', EstadoTicketController::class);
-Route::apiResource('permiso', PermisoController::class);
-Route::apiResource('prioridad-ticket', PrioridadTicketController::class);
-Route::apiResource('rol', RolController::class);
-Route::apiResource('rol-permiso', RolPermisoController::class);
-Route::apiResource('tecnico-asignado', TecnicoAsignadoController::class);
-Route::apiResource('ticket', TicketController::class);
-Route::apiResource('usuario', UsuarioController::class);
-Route::apiResource('usuario-rol', UsuarioRolController::class);
-
+Route::prefix('v1')->group(function () {
+    Route::apiResource('area', AreaController::class);
+    Route::apiResource('calificacion-ticket', CalificacionTicketController::class);
+    Route::apiResource('categoria-ticket', CategoriaTicketController::class);
+    Route::apiResource('departamento', DepartamentoController::class);
+    Route::apiResource('estado-ticket', EstadoTicketController::class);
+    Route::apiResource('permiso', PermisoController::class);
+    Route::apiResource('prioridad-ticket', PrioridadTicketController::class);
+    Route::apiResource('rol', RolController::class);
+    Route::apiResource('rol-permiso', RolPermisoController::class);
+    Route::apiResource('tecnico-asignado', TecnicoAsignadoController::class);
+    Route::apiResource('ticket', TicketController::class);
+    Route::apiResource('usuario', UsuarioController::class);
+    Route::apiResource('usuario-rol', UsuarioRolController::class);
+    Route::apiResource('bitacora', BitacoraTicketController::class);
+});

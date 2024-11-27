e8af0d55fb9697ed18d47cacb1f0f2a2a34e4ac5
Jesus <133887362+JessEleven@users.noreply.github.com>
2024-11-27 11:29:54 -0500

    accessories to format dates in models, ticket category and ticket status

--- app/Models/CategoriaTicket.php
+++ app/Models/CategoriaTicket.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class CategoriaTicket extends Model
 {
@@ -15,5 +16,16 @@ class CategoriaTicket extends Model
     public function tickets()
     {
         return $this->hasMany(Ticket::class, 'id_categoria');
+    }
+
+    // Accesor para created_at y updated_at
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->timezone(config('app.timezone'))->format('d/m/Y H:i:s');
+    }
+
+    public function getUpdatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->timezone(config('app.timezone'))->format('d/m/Y H:i:s');
     }
 }
--- app/Models/EstadoTicket.php
+++ app/Models/EstadoTicket.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class EstadoTicket extends Model
 {
@@ -17,5 +18,16 @@ class EstadoTicket extends Model
     public function tickets()
     {
         return $this->hasMany(Ticket::class, 'id_estado');
+    }
+
+    // Accesor para created_at y updated_at
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->timezone(config('app.timezone'))->format('d/m/Y H:i:s');
+    }
+
+    public function getUpdatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->timezone(config('app.timezone'))->format('d/m/Y H:i:s');
     }
 }

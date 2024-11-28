f75870b1e22133dd2440b83aa5d704ead131171a
Jesus <133887362+JessEleven@users.noreply.github.com>
2024-11-28 16:50:34 -0500

    automatically clearing the routes cache in development mode

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Para entorno dev cada vez que se cambien el nombre de las rutas
+        // desde el archivo api.php
+        if (app()->isLocal() && file_exists(base_path('bootstrap/cache/routes.php'))) {
+            // Limpiar la caché de rutas solo si está activada
+            Artisan::call('route:clear');
+        }
     }
 }

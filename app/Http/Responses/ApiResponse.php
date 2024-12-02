dc736a14e22bf08bfbd7fe59703e9e553b3ad429
Jesus <133887362+JessEleven@users.noreply.github.com>
2024-12-02 11:51:07 -0500

    new JSON type response structure, APIResponse success method

--- app/Http/Responses/ApiResponse.php
+++ app/Http/Responses/ApiResponse.php
@@ -5,6 +5,34 @@ namespace App\Http\Responses;
 class ApiResponse {
 
     public static function success($message= "Success", $statusCode= 200, $data= []) {
+
+        // Detectar si $data es una instancia de LengthAwarePaginator (cuando se usa paginación)
+        // Ya que si se busca un solo recurso "results" tomara "data"
+        if ($data instanceof \Illuminate\Pagination\LengthAwarePaginator) {
+            return response()->json([
+                "message"=> $message,
+                "status_code"=> $statusCode,
+                "error"=> false,
+                "results"=> [
+                    "pagination"=> [
+                        "current_page"=> $data->currentPage(),
+                        "last_page"=> $data->lastPage(),
+                        "per_page"=> $data->perPage(),
+                        "start_position"=> $data->firstItem(),
+                        "end_position"=> $data->lastItem(),
+                        "total"=> $data->total()
+                    ],
+                    "links"=> [
+                        "next_page_url"=> $data->nextPageUrl(),
+                        "prev_page_url"=> $data->previousPageUrl(),
+                        "path" => $data->path()
+                    ],
+                    "data" => $data->items(),
+                ],
+            ], $statusCode);
+        }
+
+        // Para un solo recurso,"data" sera "data"
         return response()->json([
             "message"=> $message,
             "status_code"=> $statusCode,
@@ -36,7 +64,7 @@ class ApiResponse {
             "message"=> $message,
             "status_code"=> $statusCode,
             "error"=> false,
-            "link"=> $data
+            "links"=> $data
         ], $statusCode);
     }
 }

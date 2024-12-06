257e681f2d2721f809e87de2e1fd366eaf6fd2b1
Jesus <133887362+JessEleven@users.noreply.github.com>
2024-12-06 10:25:35 -0500

    RESTful operations for priorities of tickets have been added

--- app/Http/Controllers/PrioridadTicketController.php
+++ app/Http/Controllers/PrioridadTicketController.php
@@ -2,51 +2,151 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Responses\ApiResponse;
 use App\Models\PrioridadTicket;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class PrioridadTicketController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        $prioridadTicket = PrioridadTicket::all();
-        return response()->json([
-            "prioridad_junin"=> $prioridadTicket
-        ]);
+        try {
+            $allPriority = PrioridadTicket::all();
+
+            if ($allPriority->isEmpty()) {
+                return ApiResponse::success(
+                    "Lista de prioridades de ticket vacia",
+                    200,
+                    $allPriority
+                );
+            }
+            return ApiResponse::success(
+                "Listado de prioridad de ticket",
+                200,
+                $allPriority
+            );
+
+        } catch (Exception $e) {
+            return ApiResponse::error(
+                "Ha ocurrido un error inesperado",
+                404
+            );
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        try {
+            $newPriorityTicket = PrioridadTicket::create([
+                "nombre_prioridad"=> $request->nombre_prioridad,
+                "color_prioridad"=> $request->color_prioridad,
+                "orden_prioridad"=> $request->orden_prioridad
+            ]);
+
+            return ApiResponse::success(
+                "Prioridad de ticket creado con exito",
+                201,
+                $newPriorityTicket
+            );
+
+        } catch (Exception $e) {
+            return ApiResponse::error(
+                "Ha ocurrido un error inesperado",
+                500
+            );
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(PrioridadTicket $prioridadTicket)
+    public function show($prioridadTicket)
     {
-        //
+        try {
+            $showPriorityTicket = PrioridadTicket::findOrFail($prioridadTicket);
+
+            return ApiResponse::success(
+                "Prioridad ticket encontrado con exito",
+                200,
+                $showPriorityTicket
+            );
+
+        } catch (ModelNotFoundException $e) {
+            return ApiResponse::error(
+                "Prioridad ticket no encontrado",
+                404
+            );
+
+        } catch (Exception $e) {
+            return ApiResponse::error(
+                "Ha ocurrido un error inesperado",
+                500
+            );
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, PrioridadTicket $prioridadTicket)
+    public function update(Request $request, $prioridadTicket)
     {
-        //
+        try {
+            $updatePriorityTicket = PrioridadTicket::findOrFail($prioridadTicket);
+
+            /* $newData = collect($request->validated())->mapWithKeys(fn($value, $key) => [
+                $key => is_string($value) ? trim($value) : $value,
+            ])->toArray();
+
+            $existingData = collect($updatePriorityTicket->only(array_keys($newData)))->mapWithKeys(fn($value, $key) => [
+                $key => is_string($value) ? trim($value) : $value,
+            ])->toArray();
+
+            if ($newData == $existingData) {
+                return ApiResponse::success(
+                    "No hay cambios para actualizar prioridad ticket",
+                    200,
+                    $newData
+                );
+            } */
+            $updatePriorityTicket->update([
+                "nombre_prioridad"=> $request->nombre_prioridad,
+                "color_prioridad"=> $request->color_prioridad
+            ]);
+
+            return ApiResponse::success(
+                "Prioridad ticket actualizado con exito",
+                200,
+                $updatePriorityTicket->refresh()
+            );
+
+        }   catch (Exception $e) {
+            return ApiResponse::error(
+                "Ha ocurrido un error inesperado",
+                500
+            );
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(PrioridadTicket $prioridadTicket)
+    public function destroy($prioridadTicket)
     {
-        //
+        try {
+            PrioridadTicket::findOrFail($prioridadTicket)->delete();
+
+            $baseRoute = $this->getBaseRoute();
+
+            return ApiResponse::deleted(
+                "Prioridad ticket eliminado con exito",
+                200,
+                ["related"=> $baseRoute]
+            );
+
+        } catch (ModelNotFoundException $e) {
+            return ApiResponse::error(
+                "Prioridad ticket no encontrado",
+                404
+            );
+
+        } catch (Exception $e) {
+            return ApiResponse::error(
+                "Ha ocurrido un error inesperado",
+                500
+            );
+        }
     }
 }

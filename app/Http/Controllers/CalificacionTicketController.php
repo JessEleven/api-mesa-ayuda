486c4257e45b2ddd33af1768cf5168c8d81e56ab
Jesus <133887362+JessEleven@users.noreply.github.com>
2024-12-09 10:46:35 -0500

    RESTful operations for ratings of tickets have been added

--- app/Http/Controllers/CalificacionTicketController.php
+++ app/Http/Controllers/CalificacionTicketController.php
@@ -2,51 +2,134 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Responses\ApiResponse;
 use App\Models\CalificacionTicket;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class CalificacionTicketController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        $calificacion = CalificacionTicket::all();
-        return response()->json([
-            "calificacion_junin"=> $calificacion
-        ]);
+        try {
+            $allQualification = CalificacionTicket::orderBy("id", "asc")->paginate(20);
+
+            if ($allQualification->isEmpty()) {
+                return ApiResponse::success(
+                    "Lista de calificaciones de ticket vacia",
+                    200,
+                    $allQualification
+                );
+            }
+            return ApiResponse::success(
+                "Listado de calificaciones de ticket",
+                200,
+                $allQualification
+            );
+        } catch (Exception $e) {
+            return ApiResponse::error(
+                "Ha ocurrido un error inesperado",
+                500
+            );
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        try {
+            $newQualificationTicket = CalificacionTicket::create([
+                "calificacion"=> $request->calificacion,
+                "observacion"=> $request->observacion
+            ]);
+
+            return ApiResponse::success(
+                "Calificación de ticket creada con exito",
+                201,
+                $newQualificationTicket
+            );
+
+        } catch (Exception $e) {
+            return ApiResponse::error(
+                "Ha ocurrido un error inesperado",
+                500
+            );
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(CalificacionTicket $calificacionTicket)
+    public function show($calificacionTicket)
     {
-        //
+        try {
+            $showQualificationTicket = CalificacionTicket::findOrFail($calificacionTicket);
+
+            return ApiResponse::success(
+                "Calificación ticket encontrada con exito",
+                200,
+                $showQualificationTicket
+            );
+
+        } catch(ModelNotFoundException $e) {
+            return ApiResponse::error(
+                "Calificación ticket no encontrada",
+                404
+            );
+
+        } catch (Exception $e) {
+            return ApiResponse::error(
+                "Ha ocurrido un error inesperado",
+                500
+            );
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, CalificacionTicket $calificacionTicket)
+    public function update(Request $request, $calificacionTicket)
     {
-        //
+        try {
+            $updateQualificationTicket = CalificacionTicket::findOrFail($calificacionTicket);
+
+            $updateQualificationTicket->update([
+                "calificacion"=> $request->calificacion,
+                "observacion"=> $request->observacion
+            ]);
+
+            return ApiResponse::success(
+                "Calificación ticket actualizada con exito",
+                200,
+                $updateQualificationTicket
+            );
+
+        } catch (Exception $e) {
+            return ApiResponse::error(
+                "Ha ocurrido un error inesperado",
+                500
+            );
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(CalificacionTicket $calificacionTicket)
+    public function destroy($calificacionTicket)
     {
-        //
+        try {
+            CalificacionTicket::findOrFail($calificacionTicket)->delete();
+
+            $baseRoute = $this->getBaseRoute();
+
+            return ApiResponse::deleted(
+                "Calificación ticket eliminada con exito",
+                200,
+                ["related"=> $baseRoute]
+            );
+
+        } catch (ModelNotFoundException $e) {
+            return ApiResponse::error(
+                "Calificación ticket no encontrada",
+                404
+            );
+
+        } catch (Exception $e) {
+            return ApiResponse::error(
+                "Ha ocurrido un error inesperado",
+                500
+            );
+        }
     }
 }

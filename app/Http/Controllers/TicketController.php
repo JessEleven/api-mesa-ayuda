95c988851e4b320c5157397704fc8403174b22ec
Jesús Mendoza <133887362+JessEleven@users.noreply.github.com>
2024-12-13 23:25:14 -0500

    RESTful operations for tickets (methods index and store) have been added

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,49 +2,81 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Responses\ApiResponse;
 use App\Models\Ticket;
+use Exception;
 use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        $ticket = Ticket::all();
-        return response()->json([
-            "ticket_junin"=> $ticket
-        ]);
+        try {
+            $allTickets = Ticket::with("usuarios", "categoria_tickets",
+            "estado_tickets", "prioridad_tickets", "calificacion_tickets")
+                ->orderBy("id", "asc")
+                ->paginate(20);
+
+            if ($allTickets->isEmpty()) {
+                return ApiResponse::success(
+                    "Lista de tickets vacia",
+                    200,
+                    $allTickets
+                );
+            }
+            return ApiResponse::success(
+                "Listado de tickets",
+                200,
+                $allTickets
+            );
+
+        } catch (Exception $e) {
+            return ApiResponse::error(
+                "Ha ocurrido un error inesperado",
+                500
+            );
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        try {
+            $newTicket = Ticket::create([
+                "codigo_ticket"=> $request->codigo_ticket,
+                "descripcion"=> $request->descripcion,
+                "fecha_inicio"=> $request->fecha_inicio,
+                "fecha_fin"=> $request->fecha_fin,
+                "id_categoria"=> $request->id_categoria,
+                "id_usuario"=> $request->id_usuario,
+                "id_estado"=> $request->id_estado,
+                "id_prioridad"=> $request->id_prioridad,
+                "id_calificacion"=> $request->id_calificacion
+            ]);
+
+            return ApiResponse::success(
+                "Ticket creado con exito",
+                201,
+                $newTicket
+            );
+
+        } catch (Exception $e) {
+            return ApiResponse::error(
+                "Ha ocurrido un error inesperado",
+                500
+            );
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Ticket $ticket)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Ticket $ticket)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Ticket $ticket)
     {
         //

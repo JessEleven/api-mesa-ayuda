e41bca090d5d87220a60c9215f4078483f7a8572
Jesús Mendoza <133887362+JessEleven@users.noreply.github.com>
2024-12-17 22:31:44 -0500

    new fields were added area to seeder

--- database/seeders/Organizacion/AreaSeeder.php
+++ database/seeders/Organizacion/AreaSeeder.php
@@ -15,22 +15,32 @@ class AreaSeeder extends Seeder
         $areas = [
             [
                 'nombre_area'=> 'Finanzas',
+                'sigla_area'=> 'F',
+                'secuencia_area'=> '001-F',
                 'peso_prioridad'=> '1',
             ],
             [
                 'nombre_area'=> 'Obras Públicas',
+                'sigla_area'=> 'OP',
+                'secuencia_area'=> '002-OP',
                 'peso_prioridad'=> '2',
             ],
             [
                 'nombre_area'=> 'Desarrollo Social',
+                'sigla_area'=> 'DS',
+                'secuencia_area'=> '003-DS',
                 'peso_prioridad'=> '3',
             ],
             [
                 'nombre_area'=> 'Seguridad Ciudadana',
+                'sigla_area'=> 'SC',
+                'secuencia_area'=> '004-SC',
                 'peso_prioridad'=> '4',
             ],
             [
                 'nombre_area'=> 'Medio Ambiente',
+                'sigla_area'=> 'MA',
+                'secuencia_area'=> '005-MA',
                 'peso_prioridad'=> '5',
             ]
         ];

10016e6f8319ce12f6de05007c9c62eba1cbd39d
Jesús Mendoza <133887362+JessEleven@users.noreply.github.com>
2024-12-30 11:50:04 -0500

    RESTful operations for assigned technicians have been added

--- app/Http/Controllers/TecnicoAsignadoController.php
+++ app/Http/Controllers/TecnicoAsignadoController.php
@@ -2,51 +2,139 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Responses\ApiResponse;
 use App\Models\TecnicoAsignado;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class TecnicoAsignadoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        $tecnicoAsiganado = TecnicoAsignado::all();
-        return response()->json([
-            "tecnico_asiganado_junin"=> $tecnicoAsiganado
-        ]);
+        try {
+            $allTechnicians = TecnicoAsignado::with("usuarios")
+                ->with("tickets")
+                    ->get();
+
+            if ($allTechnicians->isEmpty()) {
+                return ApiResponse::success(
+                    "Lista de técnicos asignado vacía",
+                    200,
+                    $allTechnicians
+                );
+            }
+            return ApiResponse::success(
+                "Listado de técnicos asignado",
+                200,
+                $allTechnicians
+            );
+
+        } catch (Exception $e) {
+            return ApiResponse::error(
+                "Ha ocurrido un error inesperado",
+                500
+            );
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        try {
+            $newTechnician = TecnicoAsignado::create([
+                "id_usuario"=> $request->id_usuario,
+                "id_ticket"=> $request->id_ticket
+            ]);
+
+            return ApiResponse::success(
+                "Técnico asignado creado con éxito",
+                201,
+                $newTechnician
+            );
+
+        } catch (Exception $e) {
+            return ApiResponse::error(
+                "Ha ocurrido un error inesperado",
+                500
+            );
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(TecnicoAsignado $tecnicoAsignado)
+    public function show($tecnicoAsignado)
     {
-        //
+        try {
+            $showTechnician = TecnicoAsignado::with("usuarios")
+                ->with("tickets")
+                    ->findOrFail($tecnicoAsignado);
+
+            return ApiResponse::success(
+                "Técnico asignado encontrado con éxito",
+                201,
+                $showTechnician
+            );
+
+        } catch (ModelNotFoundException $e) {
+            return ApiResponse::error(
+                "Técnico asignado no encontrado",
+                404
+            );
+
+        } catch (Exception $e) {
+            return ApiResponse::error(
+                "Ha ocurrido un error inesperado",
+                500
+            );
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, TecnicoAsignado $tecnicoAsignado)
+    public function update(Request $request, $tecnicoAsignado)
     {
-        //
+        try {
+            $updateTechnician = TecnicoAsignado::findOrFail($tecnicoAsignado);
+
+            $updateTechnician->update([
+                "id_usuario"=> $request->id_usuario,
+                "id_ticket"=> $request->id_ticket
+            ]);
+
+            return ApiResponse::success(
+                "Técnico asignado actualizado con éxito",
+                200,
+                $updateTechnician
+            );
+
+        } catch (Exception $e) {
+            return ApiResponse::error(
+                "Ha ocurrido un error inesperado",
+                500
+            );
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(TecnicoAsignado $tecnicoAsignado)
+    public function destroy($tecnicoAsignado)
     {
-        //
+        try {
+            TecnicoAsignado::findOrFail($tecnicoAsignado)->delete();
+
+            $baseRoute = $this->getBaseRoute();
+
+            return ApiResponse::deleted(
+                "Técnico asignado eliminado con éxito",
+                200,
+                ["related"=> $baseRoute]
+            );
+
+        } catch (ModelNotFoundException $e) {
+            return ApiResponse::error(
+                "Técnico asignado no encontrado",
+                404
+            );
+
+        } catch (Exception $e) {
+            return ApiResponse::error(
+                "Ha ocurrido un error inesperado",
+                500
+            );
+        }
     }
 }

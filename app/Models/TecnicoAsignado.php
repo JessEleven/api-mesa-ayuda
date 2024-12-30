9bc70276d79675a35ccc251ca7bda963ae7bc7dc
Jesús Mendoza <133887362+JessEleven@users.noreply.github.com>
2024-12-30 12:04:40 -0500

    accessories to format dates in the model of assigned technician

--- app/Models/TecnicoAsignado.php
+++ app/Models/TecnicoAsignado.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Carbon;
 
 class TecnicoAsignado extends Model
 {
@@ -29,5 +30,16 @@ class TecnicoAsignado extends Model
     public function tickets()
     {
         return $this->belongsTo(Ticket::class, 'id_ticket');
+    }
+
+    // Accesor para created_at y updated_at
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->timezone(config('app.timezone'))->format('d/m/Y H:i:s');
+    }
+
+    public function getUpdatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->timezone(config('app.timezone'))->format('d/m/Y H:i:s');
     }
 }

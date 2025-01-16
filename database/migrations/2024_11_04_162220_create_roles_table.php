40d97329b64c9d39855426b201ac313e44120abc
Jesús Mendoza <133887362+JessEleven@users.noreply.github.com>
2025-01-16 10:45:46 -0500

    changed softDeletes trait to a custom field in role migration

--- database/migrations/2024_11_04_162220_create_roles_table.php
+++ database/migrations/2024_11_04_162220_create_roles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('nombre_rol');
-            $table->softDeletes();
+            $table->timestamp('recurso_eliminado')->nullable();
             $table->timestamps();
         });
     }

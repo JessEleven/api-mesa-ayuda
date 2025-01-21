525f08910335b6490884708f81227568a74f61c0
Jesús Mendoza <133887362+JessEleven@users.noreply.github.com>
2025-01-21 15:20:59 -0500

    added the methods and accessory to format date for the custom field in the ticket binnacle model

--- app/Models/BitacoraTicket.php
+++ app/Models/BitacoraTicket.php
@@ -3,13 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Carbon;
 
 class BitacoraTicket extends Model
 {
-    use SoftDeletes;
-
     protected $table = 'bitacoras_tickets';
 
     protected $fillable = [
@@ -17,9 +14,35 @@ class BitacoraTicket extends Model
         'id_tecnico_asignado'
     ];
 
+    // Para marcar un registro como eliminado
+    public function delete()
+    {
+        $this->recurso_eliminado = now();
+        $this->save();
+    }
+
+    // Por si se quiere restaurar un registro eliminado
+    public function restore()
+    {
+        $this->recurso_eliminado = null;
+        $this->save();
+    }
+
+    // Para verificar si el registro está eliminado
+    public function trashed()
+    {
+        return !is_null($this->recurso_eliminado);
+    }
+
     public function tecnico_asigados()
     {
         return $this->belongsTo(TecnicoAsignado::class, 'id_tecnico_asignado');
+    }
+
+    // Accesor para registro_eliminado
+    public function getRecursoEliminadoAttribute($value)
+    {
+        return $value ? Carbon::parse($value)->timezone(config('app.timezone'))->format('d/m/Y H:i:s') : null;
     }
 
     // Accesor para created_at y updated_at

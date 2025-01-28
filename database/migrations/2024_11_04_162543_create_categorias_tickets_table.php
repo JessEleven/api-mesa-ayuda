bbdcd427fd9f721aad926528ccf7f5299377e35c
Jesús Mendoza <133887362+JessEleven@users.noreply.github.com>
2025-01-28 11:41:40 -0500

    added a new field (sigla_categoria) in ticket categories migration

--- database/migrations/2024_11_04_162543_create_categorias_tickets_table.php
+++ database/migrations/2024_11_04_162543_create_categorias_tickets_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('categorias_tickets', function (Blueprint $table) {
             $table->id();
             $table->string('nombre_categoria')->unique();
+            $table->string('sigla_categoria')->unique();
             $table->timestamps();
         });
     }

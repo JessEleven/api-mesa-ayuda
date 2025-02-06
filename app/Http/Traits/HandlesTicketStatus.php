32795d9d65915b7beb28d4b8d7b6247c98804795
Jesús Mendoza <133887362+JessEleven@users.noreply.github.com>
2025-02-05 21:31:56 -0500

    a Trait was created to validate if a ticket has ended

---
+++ app/Http/Traits/HandlesTicketStatus.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Traits;
+
+use App\Http\Responses\ApiResponse;
+use App\Models\EstadoTicket;
+use App\Models\Ticket;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+trait HandlesTicketStatus
+{
+    public function ticketIsFinalized($ticket)
+    {
+        // Se verifica si el ID ingresado existe
+        $ticketId = Ticket::find($ticket);
+
+        if (!$ticketId) {
+            throw new HttpResponseException(ApiResponse::error(
+                "Ticket no encontrado",
+                404
+            ));
+        }
+
+        // Se obtiene el estado con el orden de prioridad más alto que existe
+        $maxPriority = EstadoTicket::max("orden_prioridad");
+
+        // Se busca el estado actual que tiene el ticket
+        $currentStatus = EstadoTicket::find($ticketId->id_estado);
+
+        // Si el ticket está en el estado con la mayor orden de prioridad no se podra
+        // editar (UpdateTicketRequest.php) o eliminar (TicketController.php)
+        if ($currentStatus && $currentStatus->orden_prioridad === $maxPriority) {
+            throw new HttpResponseException(ApiResponse::error(
+                "El ticket ha sido finalizado",
+                400,
+                ["current_status"=> $currentStatus->nombre_estado]
+            ));
+        }
+        return $ticketId;
+    }
+}
